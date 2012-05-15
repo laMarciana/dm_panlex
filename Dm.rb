f34c5a6b3a53ecae6f77928e4e523d20364d37ac
marc <cram1010@gmail.com>
2012-05-15 15:40:44 +0200

    Dm properties

---
+++ Dm.rb
@@ -0,0 +1,9 @@
+class Dm
+   include DataMapper::Resource
+
+   storage_names[:default] = 'dm'
+
+   property :dm, Integer, :key => true
+   property :mn, Integer, :required => true, :unique_index => :dm_mn_key
+   property :ex, Integer, :required => true, :unique_index => :dm_mn_key
+end
