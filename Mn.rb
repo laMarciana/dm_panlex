8e25152d54c47f85fb2c79f4924098a07125e24b
marc <cram1010@gmail.com>
2012-05-17 13:37:17 +0200

    Mn relations

--- Mn.rb
+++ Mn.rb
@@ -5,4 +5,11 @@ class Mn
 
    property :mn, Integer, :key => true
    property :ap, Integer, :required => true, :index => true
+
+   belongs_to :ap, :parent_key => :ap, :child_key  => :ap
+
+   has n, :df, :parent_key => :mn, :child_key => :mn
+   has n, :dm, :parent_key => :mn, :child_key => :mn
+   has n, :dn, :parent_key => :mn, :child_key => :mn
+   has n, :mi, :parent_key => :mn, :child_key => :mn
 end
