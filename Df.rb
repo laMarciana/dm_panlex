c4ec563772f221a0ad954a23f53ff8f86773f1a5
marc <cram1010@gmail.com>
2012-05-17 12:45:10 +0200

    Df relations

--- Df.rb
+++ Df.rb
@@ -7,4 +7,7 @@ class Df
    property :mn, Integer, :required => true, :unique_index => :df_mn_key
    property :lv, Integer, :required => true, :unique_index => :df_mn_key
    property :tt, Text, :required => true, :unique_index => :df_mn_key
+
+   belongs_to :lv, :parent_key => :lv, :child_key  => :lv
+   belongs_to :mn, :parent_key => :mn, :child_key  => :mn
 end
