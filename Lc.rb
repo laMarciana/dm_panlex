aadd084e2d430ff37199f5f7870f81183a6df816
marc <cram1010@gmail.com>
2012-05-17 13:30:18 +0200

    Lc relations

--- Lc.rb
+++ Lc.rb
@@ -5,4 +5,7 @@ class Lc
 
    property :lc, String, :length => 3, :key => true
    property :tp, String, :length => 1, :required => true
+
+   has n, :i1, :parent_key => :lc, :child_key => :iso3
+   has n, :lv, :parent_key => :lc, :child_key => :lc
 end
