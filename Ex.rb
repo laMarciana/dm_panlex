31c347848a2485450c878f64b5f543d13e13cf31
marc <cram1010@gmail.com>
2012-05-17 12:51:29 +0200

    Ex relations

--- Ex.rb
+++ Ex.rb
@@ -7,4 +7,9 @@ class Ex
    property :lv, Integer, :unique_index => :ex_lv_tt_key, :index => true
    property :tt, Text, :unique_index => :ex_lv_tt_key, :index => true
    property :td, Text, :index => true
+
+   belongs_to :lv, :parent_key => :lv, :child_key  => :lv
+
+   has n, :dm, :parent_key => :ex, :child_key => :ex
+   has n, :dn, :parent_key => :ex, :child_key => :ex
 end
