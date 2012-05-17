885c7e5fc36d039fe7436da2a01ffc9e1a15820f
marc <cram1010@gmail.com>
2012-05-17 17:37:53 +0200

    Document Panlex.rb

--- Cp.rb
+++ Cp.rb
@@ -7,5 +7,5 @@ class Cp
    property :c0, String, :length => 5, :key => true
    property :c1, String, :length => 5, :required => true, :unique_index => :cp_lv_key
 
-   belongs_to :lv, :parent_key => :lv, :child_key  => :lv#, :key => true
+   belongs_to :lv, :parent_key => :lv, :child_key  => :lv
 end
