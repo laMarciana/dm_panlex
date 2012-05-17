92ce2136bc80971201ecaf2af01d624dd2b04dcc
marc <cram1010@gmail.com>
2012-05-17 13:39:23 +0200

    Wc relations

--- Wc.rb
+++ Wc.rb
@@ -6,4 +6,7 @@ class Wc
    property :wc, Integer, :key => true
    property :dn, Integer, :required => true, :unique_index => :wc_dn_key
    property :ex, Integer, :required => true, :unique_index => :wc_dn_key
+
+   belongs_to :dn, :parent_key => :dn, :child_key  => :dn
+   belongs_to :wcex, :parent_key => :ex, :child_key  => :ex
 end
