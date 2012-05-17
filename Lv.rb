db030b9f687ad67ddd93a4ff7e715b3ca6d59082
marc <cram1010@gmail.com>
2012-05-17 13:32:44 +0200

    Lv references

--- Lv.rb
+++ Lv.rb
@@ -9,4 +9,12 @@ class Lv
    property :sy, Boolean, :required => true, :default => true
    property :am, Boolean, :required => true, :default => true
    property :tt, Text, :required => true
+
+   belongs_to :lc, :parent_key => :lc, :child_key  => :lc
+
+   has n, :av, :parent_key => :lv, :child_key => :lv
+   has n, :cp, :parent_key => :lv, :child_key => :lv
+   has n, :cu, :parent_key => :lv, :child_key => :lv
+   has n, :df, :parent_key => :lv, :child_key => :lv
+   has n, :ex, :parent_key => :lv, :child_key => :lv
 end
