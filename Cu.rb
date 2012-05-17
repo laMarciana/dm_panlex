2a1a2ed9bc8b386211551e558c62a689afede73e
marc <cram1010@gmail.com>
2012-05-17 12:44:18 +0200

    Cu relations

--- Cu.rb
+++ Cu.rb
@@ -9,4 +9,6 @@ class Cu
    property :c1, String, :length => 5, :required => true, :unique_index => :cu_c1_key
    property :loc, Text, :key => true, :unique_index => [:cu_c0_key, :cu_c1_key]
    property :vb, Text, :key => true, :required => true, :unique_index => [:cu_c0_key, :cu_c1_key]
+
+   belongs_to :lv, :parent_key => :lv, :child_key  => :lv
 end
