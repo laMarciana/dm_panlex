7e9ead74c1bda1f667a0cb08024ad8b7e4bcc7ac
marc <cram1010@gmail.com>
2012-05-17 12:46:06 +0200

    Dm relations

--- Dm.rb
+++ Dm.rb
@@ -6,4 +6,7 @@ class Dm
    property :dm, Integer, :key => true
    property :mn, Integer, :required => true, :unique_index => :dm_mn_key
    property :ex, Integer, :required => true, :unique_index => :dm_mn_key
+
+   belongs_to :ex, :parent_key => :ex, :child_key  => :ex
+   belongs_to :mn, :parent_key => :mn, :child_key  => :mn
 end
