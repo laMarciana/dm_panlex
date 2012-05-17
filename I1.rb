de8629b1193b6a75b4de742994a892591b71c0fb
marc <cram1010@gmail.com>
2012-05-17 13:28:54 +0200

    I1 relations

--- I1.rb
+++ I1.rb
@@ -4,5 +4,7 @@ class I1
    storage_names[:default] = 'i1'
 
    property :iso1, String, :length => 2, :key => true
-   property :iso2, String, :length => 3, :required => true
+   property :iso3, String, :length => 3, :required => true
+
+   belongs_to :lc, :parent_key => :lc, :child_key  => :iso3
 end
