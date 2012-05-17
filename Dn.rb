4c6b621828318a83624a6aeec13729b75cce0377
marc <cram1010@gmail.com>
2012-05-17 12:49:44 +0200

    Dn relations

--- Dn.rb
+++ Dn.rb
@@ -6,4 +6,10 @@ class Dn
    property :dn, Integer, :key => true
    property :mn, Integer, :required => true, :unique_index => :dm_mn_ex_key, :index => true
    property :ex, Integer, :required => true, :unique_index => :dm_mn_ex_key, :index => true
+
+   belongs_to :ex, :parent_key => :ex, :child_key  => :ex
+   belongs_to :mn, :parent_key => :mn, :child_key  => :mn
+
+   has n, :md, :parent_key => :dn, :child_key => :dn
+   has n, :wc, :parent_key => :dn, :child_key => :dn
 end
