fd461841bb039b90077a1660766e17f89d17d677
marc <marc@lamarciana.com>
2012-07-20 11:10:03 +0200

    cu(loc) & cu(vb) to String to work as PK in MySQL

--- lib/panlex/models/cu.rb
+++ lib/panlex/models/cu.rb
@@ -3,12 +3,12 @@ class Panlex::Cu
 
    storage_names[:default] = 'cu'
 
-   #DataMapper requires a primary Key. It's not present in Panlex original database, but we assume it's the :cu_c0_key unique index
+   #DataMapper requires a primary Key. It's not present in Panlex original database, but we assume it's the :cu_c0_key unique index. In MySQL a 'Text' field can't be a PK, so we change 'loc' and 'vb' to be 'String'
    property :lv, Integer, :key => true, :required => true, :unique_index => [:cu_c0_key, :cu_c1_key]
    property :c0, String, :length => 5, :key => true, :required => true, :unique_index => :cu_c0_key
    property :c1, String, :length => 5, :required => true, :unique_index => :cu_c1_key
-   property :loc, Text, :key => true, :unique_index => [:cu_c0_key, :cu_c1_key]
-   property :vb, Text, :key => true, :required => true, :unique_index => [:cu_c0_key, :cu_c1_key]
+   property :loc, String, :length => 255, :key => true, :unique_index => [:cu_c0_key, :cu_c1_key]
+   property :vb, String, :length => 255, :key => true, :required => true, :unique_index => [:cu_c0_key, :cu_c1_key]
 
    belongs_to :lv, :parent_key => :lv, :child_key  => :lv
-end
+end 
--- lib/panlex/panlex.rb
+++ lib/panlex/panlex.rb
@@ -3,7 +3,7 @@
 #
 # - ap(dt) and us(dt), which are meant to store current data when creating a record, are set by the model (using dm-timestamp) instead that by the database
 # - 'ON UPDATE CASCADE' in wcex-wc 1-n relatio is not implemented because, I far as I know, dm-constraints can't do that without adding 'ON DELETE CASCADE', too.
-# - A PK for cu(lv, co, loc, vb) is added because datamapper requires each model to have a PK. That makes cu(loc) field to be a required one even if it's not in original Panlex database
+# - A PK for cu(lv, co, loc, vb) is added because datamapper requires each model to have a PK. That makes cu(loc) field to be a required one even if it's not in original Panlex database. It forces as well cu(loc) and cu(loc) to be String instead of Text, as in MySQL a Text can't be a PK
 # - fields with 'character' type are changed to 'character varying'
 # - PostgreSQL Cluster (http://www.postgresql.org/docs/8.1/static/sql-cluster.html) are not implemented
 # - Unique indexes appear as 'UNIQUE' instead of 'UNIQUE CONSTRAINT' (don't know why)
