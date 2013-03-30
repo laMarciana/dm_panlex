0b5f3c3b706cc59220d56613fb00d1a62c5a5983
marc <marc@lamarciana.com>
2013-03-30 17:41:59 +0100

    Spec wcex

---
+++ spec/panlex/models/wcex_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+module Panlex
+   describe Wcex, "Word classification expression" do
+      it "has a property expression (ex)" do
+         should have_property :ex
+      end
+      it "has a property text (tt)" do
+         should have_property :tt
+      end
+      it "is not valid without an expression" do
+         should validate_presence_of :ex
+      end
+      it "is not valid without a text" do
+         should validate_presence_of :tt
+      end
+      it "has many word clasifications (wc)" do
+         should have_many :wcs
+      end
+   end
+end
