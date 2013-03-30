33bf123488634826be1f7748b08a79d9da002295
marc <marc@lamarciana.com>
2013-03-30 16:15:04 +0100

    spec Ap

--- spec/panlex/models/lc_spec.rb
+++ spec/panlex/models/lc_spec.rb
@@ -2,29 +2,23 @@ require 'spec_helper'
 
 module Panlex
    describe Lc, "Language code" do
-      describe "properties" do
-         it "has a code (lc)" do
-            should have_property :lc
-         end
-         it "has a code type (tp)" do
-            should have_property :tp
-         end
+      it "has a property code (lc)" do
+         should have_property :lc
       end
-      describe "constraints" do
-         it "is not valid without a code" do
-            should validate_presence_of :lc
-         end
-         it "is not valid without a code type" do
-            should validate_presence_of :tp
-         end
+      it "has a property code type (tp)" do
+         should have_property :tp
       end
-      describe "associations" do
-         it "has many language varieties (lv)" do
-            should have_many :lvs
-         end
-         it "has many iso codes (i1)" do
-            should have_many :i1s
-         end
+      it "is not valid without a code" do
+         should validate_presence_of :lc
+      end
+      it "is not valid without a code type" do
+         should validate_presence_of :tp
+      end
+      it "has many language varieties (lv)" do
+         should have_many :lvs
+      end
+      it "has many iso codes (i1)" do
+         should have_many :i1s
       end
    end
 end
