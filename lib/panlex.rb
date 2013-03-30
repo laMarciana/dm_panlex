cf128ea4edada3ffff29f17dd8f8d0de925ea171
marc <marc@lamarciana.com>
2013-03-30 10:50:57 +0100

    require models from main lib file

--- lib/panlex.rb
+++ lib/panlex.rb
@@ -1,3 +1,19 @@
+# This package is an ORM mapping of Panlex (http://panlex.org) database using Ruby Datamapper (http://datamapper.org)
+# Following are the only differences between the database created by this package (tested in PostgreSQL) and actual Panlex database:
+#
+# - ap(dt) and us(dt), which are meant to store current data when creating a record, are set by the model (using dm-timestamp) instead that by the database
+# - 'ON UPDATE CASCADE' in wcex-wc 1-n relatio is not implemented because, I far as I know, dm-constraints can't do that without adding 'ON DELETE CASCADE', too.
+# - A PK for cu(lv, co, loc, vb) is added because datamapper requires each model to have a PK. That makes cu(loc) field to be a required one even if it's not in original Panlex database. It forces as well cu(loc) and cu(loc) to be String instead of Text, as in MySQL a Text can't be a PK
+# - fields with 'character' type are changed to 'character varying'
+# - PostgreSQL Cluster (http://www.postgresql.org/docs/8.1/static/sql-cluster.html) are not implemented
+# - Unique indexes appear as 'UNIQUE' instead of 'UNIQUE CONSTRAINT' (don't know why)
+# - 'index' are created for fields that are FK and haven't any index in the original Panlex database
+# - Unique indexes are named 'unique_*' instead of '*_key'
+# - FK are named '*_fk' instead of '_fkey'
+# - when showing a table schema from the database, fields that 'belong to' some other table are listed as the last ones
+#
+# TO DO: Solve above points if some one is really needed
+
 %w[
    data_mapper
    dm-core
@@ -7,4 +23,10 @@
    require lib
 end
 
-require_relative 'panlex/panlex'
+module Panlex
+   Dir.glob(File.dirname(__FILE__) + '/panlex/models/*.rb') do |file|
+      require file
+   end
+
+   DataMapper.finalize
+end
